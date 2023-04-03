5ac3f5b2ca7dc43f51a62fc72448cb00a6a65571
Divyansh21321 <divyansh21321@iiitd.ac.in>
2023-04-03 20:00:55 +0530

    update homePage elements and add more interactions

--- index.js
+++ index.js
@@ -27,3 +27,37 @@ menuButton.addEventListener('click',function(){
     navMenu.classList.toggle('off');
 })
 
+let typing = new Typed('.change-words',{
+    strings : ['UI/UX Designer','Web Developer','Competitive Coder'],
+    loop : true,
+    typeSpeed : 90,
+    backSpeed : 80,
+    backDelay : 1500
+
+})
+
+// onclickMore = document.querySelector('#clickMore');
+// onclickMore.addEventListener('click',function(){
+
+//     document.getElementById('content-add').innerHTML= "I have gained experience in languages and frameworks such as HTML, CSS, JavaScript, React, and Node.js.\
+//     I am also comfortable using tools like Git, GitHub, and VS Code.<br>\
+//     "
+//     document.getElementById('clickMore').innerHTML= "<i class='bx bx-chevron-up'></i>"
+
+// })
+
+function moreClicked() {
+    var dot = document.getElementById("dot");
+    var moreContent = document.getElementById("content-add");
+    var btnDisplay = document.getElementById("clickMore");
+  
+    if (dot.style.display === "none") {
+      dot.style.display = "inline";
+      btnDisplay.innerHTML = "More...";
+      moreContent.style.display = "none";
+    } else {
+      dot.style.display = "none";
+      btnDisplay.innerHTML = "<i class='bx bx-chevron-up'></i>";
+      moreContent.style.display = "inline";
+    }
+  }
